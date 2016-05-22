b715fce479450fd5a1d0df7ecf25178f72192cce
asierculebras <asierculebras@gmail.com>
2016-05-22 17:01:06 +0200

    Tema 18: Añadir comentarios

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -84,7 +84,7 @@ exports.create = function(req, res, next) {
 
 //Autoload el quiz asociado a :quizId
 exports.load = function(req, res, next, quizId){
-	models.Quiz.findById(quizId)
+	models.Quiz.findById(quizId, { include: [ models.Comment ] })
 		.then(function(quiz){
 			if (quiz){
 				req.quiz = quiz;
--- models/index.js
+++ models/index.js
@@ -27,8 +27,17 @@ var sequelize = new Sequelize(url,
 // Importar la definicion de la tabla Quiz de quiz.js
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 
+// Importar la definicion de la tabla Comments de comment.js
+var Comment = sequelize.import(path.join(__dirname,'comment'));
+
+// Relaciones entre modelos
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
+
+/* Cambiamos a migracines y Seeders
 
 // sequelize.sync() crea e inicializa tabla de preguntas en DB
+
 sequelize.sync()
     .then(function() {
         // Ya se han creado las tablas necesarias.
@@ -50,6 +59,7 @@ sequelize.sync()
         console.log("Error Sincronizando las tablas de la BBDD:", error);
         process.exit(1);
     });
-
+*/
 
 exports.Quiz = Quiz; // exportar definición de tabla Quiz
+exports.Comment = Comment; // exportar definición de tabla de los comentarios.
